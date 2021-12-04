8ce5f5e48f207fa67a66dc962bb42fa7b5b5ed8a
ShiroKatake <tgkhang262@gmail.com>
2021-12-05 02:07:54 +1100

    UPDATE: Added access control allow origin

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ app.use(express.json());
 app.use((_, res, next) => {
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Access-Control-Allow-Origin", "https://shirokatake.github.io/ytdl-frontend/");
   next();
 });
 
