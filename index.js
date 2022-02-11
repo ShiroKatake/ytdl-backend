da66911b576a0d79585d7eae93a2525c01a95b5a
ShiroKatake <tgkhang262@gmail.com>
2022-02-12 01:44:46 +1100

    UPDATE: Moved content disposition header to same function

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ wss.on("connection", ws => {
   ws.send(ws.id);
 });
 
-app.use(cors({ exposedHeaders: ["Content-Disposition"] }));
 app.use((_, res, next) => {
   res.header('Access-Control-Allow-Origin', "https://shirokatake.github.io");
   res.header('Access-Control-Allow-Methods', 'GET,POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Expose-Headers', 'Content-Disposition');
   next();
 });
 
