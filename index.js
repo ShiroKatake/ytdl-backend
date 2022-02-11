836200ecbb869e5f297a7548f4c3e0cb4a56f3df
ShiroKatake <tgkhang262@gmail.com>
2022-02-12 00:01:18 +1100

    UPDATE: Added access control allow headers

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use((_, res, next) => {
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Access-Control-Allow-Origin", "https://shirokatake.github.io");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
