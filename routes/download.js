b58c0359f496d5a948dc1dbd9d8e4cfc930b015c
ShiroKatake <tgkhang262@gmail.com>
2022-02-13 14:16:25 +1100

    UPDATE: Changed download method from post to get

--- routes/download.js
+++ routes/download.js
@@ -9,7 +9,7 @@ const ytdl = require("ytdl-core");
 const { audioEncodeConfig, encodeOptions, videoEncodeConfig } = require("../utils/ffmpeg");
 const { generateDownloadPath, getUniqueID, CLIENTS } = require("../utils/helpers");
 
-router.post("/download", async (req, res) => {
+router.get("/download", async (req, res) => {
   const { v: url, format: format } = req.query;
   if (!ytdl.validateID(url) && !ytdl.validateURL(url)) {
     return res.status(400).json({ success: false, error: "Not a valid YouTube Id!" });
@@ -60,7 +60,7 @@ router.post("/download", async (req, res) => {
     }
 
     ffmpegProcess.stdio[3].on("data", () => {
-      CLIENTS[req.body.uid].send(
+      CLIENTS[req.query.uid].send(
         JSON.stringify({
           downloaded: tracker.audio.downloaded + tracker.video.downloaded,
           total: tracker.audio.total + tracker.video.total,
