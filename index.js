fdada64fec80308fb6e69cfcf9ac8444a590cc19
ShiroKatake <tgkhang262@gmail.com>
2022-08-20 16:50:50 +1000

    HOTFIX: Use fork of ytdl to apply fix until PR gets approved

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 app.use(json());
 app.use((req, res, next) => {
   const allowedOrigins = [
-    // "http://localhost:3000",
+    "http://localhost:3000",
     "https://shirokatake.github.io",
     "https://shirokatake-ytdl-frontend-test.herokuapp.com/"
   ];
